e9909f9a3dcf9ce3f4961dba3fb027231fb34d49
AlexAdkins1 <158346894+AlexAdkins1@users.noreply.github.com>
2024-03-09 11:51:35 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,13 @@ import java.io.FileNotFoundException;
 @SuppressWarnings("unused")
 public class Main {
     
+    /**
+     * class fields to quickly input the needed data
+     */
     private final static String FILE2020 = "2020.csv";
     private final static String FILE2021 = "2021.csv";
     private final static String FILE2022 = "2022.csv";
-    private final static String FILEOFFICERS = "County_Officers.csv";
+    private final static String FILEOFFICERS = "County_Officer_Pop.csv";
     private final static List<String> FILES = List.of(
         FILE2020,
         FILE2021,
@@ -16,24 +19,36 @@ public class Main {
         FILEOFFICERS
     );
 
+
     private TreeMap<String, TreeMap<ForceType, Integer>> forceMap = new TreeMap<>();
 
     /**
      * Constructs a new instance of the Main class.
      */
-    public Main(){
-
-    }
+    public Main(){}
 
     /**
      * @return the force map
      */
     public TreeMap<String, TreeMap<ForceType, Integer>> getForceMap() {return forceMap;}
 
+    /**
+     * helper fubction for readFile() that creates the inner map for forceMap
+     * 
+     * @param fields the ordered and uniform data of force incidents for given county
+     * @return innter force map
+     */
     private TreeMap<ForceType, Integer> innerMapMaker(String[] fields) {
         TreeMap<ForceType, Integer> map = new TreeMap<>();
         ForceType[] forceTypeList = ForceType.values();
+        for (String f: fields) {
+            System.out.println(f);
+        }
         for (int i=0; i < forceTypeList.length; i++) {
+            if (fields[i+1].equals("")) {
+                map.put(forceTypeList[i], null);
+                continue;
+            }
             map.put(forceTypeList[i], Integer.parseInt(fields[i+1]));
         }
         return map;
@@ -45,7 +60,7 @@ public class Main {
      * @param file the file to read
      * @throws FileNotFoundException if the file is not found
      */
-    public void readFile(File file) throws FileNotFoundException{
+    public void createForceMap(File file) throws FileNotFoundException{
         Scanner in = new Scanner(file);
         in.nextLine();
         while (in.hasNextLine()) {
@@ -58,13 +73,17 @@ public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
-        String CURRENTFILE = FILE2020; // for file change
+        String CURRENTFILE = FILE2022; // for file change
         try {
-            main.readFile(new File("src\\data\\" + CURRENTFILE));
+            main.createForceMap(new File("src\\data\\" + CURRENTFILE));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.out.println("Wah Wah");
         }
-        System.out.println(main.getForceMap());
+        for (Map.Entry<String, TreeMap<ForceType, Integer>> entry: main.getForceMap().entrySet()) {
+            String key = entry.getKey();
+            TreeMap<ForceType, Integer> value = entry.getValue();
+            System.out.println(key + ": \n" + value);
+        }
     }
 }
