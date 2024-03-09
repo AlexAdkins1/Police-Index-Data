be6c129ba8348c631739608eae3397d84f8008c1
Lukas May <jlukasmay@gmail.com>
2024-03-09 01:46:19 -0500

    County PD Enum

---
+++ src/main/java/Counties.java
@@ -0,0 +1,82 @@
+import java.util.List;
+
+
+public enum Counties {
+    ALBANY(List.of("Albany City PD", "Albany County Sheriff", "Altamont Vg PD", "Bethlehem Town PD", "Coeymans Town PD", "Cohoes City PD", "Colonie Town PD", "Green Island Vg PD", "Guilderland Town PD", "Menands Vg PD", "SUNY Police - Albany", "SUNY Police - Nanoscale", "Watervliet City PD")),
+    ALLEGANY(List.of("Alfred Vg PD", "Allegany County Sheriff", "Andover Vg PD", "Angelica Vg PD", "Belmont Vg PD", "Bolivar Vg PD", "Cuba Town PD", "Friendship Town PD", "Independence Town PD", "SUNY Police - Alfred", "Wellsville Vg PD")),
+    BROOME(List.of("Binghamton City PD", "Broome County Sheriff", "Deposit Vg PD", "Endicott Vg PD", "Johnson City Vg PD", "Port Dickinson Vg PD", "SUNY Police - Binghamton", "Vestal Town PD")),
+    CATTARAUGUS(List.of("Allegany Vg PD", "Cattaraugus County Sheriff", "Cattaraugus Vg PD", "Ellicottville Town PD", "Franklinville Vg PD", "Olean City PD", "Portville Vg PD", "Salamanca City PD")),
+    CAYUGA(List.of("Auburn City PD", "Cayuga County Sheriff", "Moravia Vg PD", "Port Byron Vg PD", "Weedsport Vg PD")),
+    CHAUTAUQUA(List.of("Carroll Town PD", "Chautauqua County Sheriff", "Dunkirk City PD", "Ellicott Town PD", "Fredonia Vg PD", "Jamestown City PD", "Lakewood-Busti PD", "Silver Creek Vg PD", "SUNY Police - Fredonia", "Westfield Vg PD")),
+    CHEMUNG(List.of("Chemung County Sheriff", "Elmira City PD", "Elmira Heights Vg PD", "Elmira Town PD", "Horseheads Vg PD")),
+    CHENANGO(List.of("Afton Vg PD", "Bainbridge Vg PD", "Chenango County Sheriff", "Greene Vg PD", "New Berlin Town PD", "Norwich City PD", "Oxford Vg PD", "Sherburne Vg PD")),
+    CLINTON(List.of("Clinton County Sheriff", "Plattsburgh City PD", "SUNY Police - Plattsburgh")),
+    COLUMBIA(List.of("Chatham Vg PD", "Columbia County Sheriff", "Copake Town PD", "Greenport Town PD", "Hudson City PD", "Philmont Vg PD", "Stockport Town PD")),
+    CORTLAND(List.of("Cortland City PD", "Cortland County Sheriff", "Homer Vg PD", "SUNY Police - Cortland")),
+    DELAWARE(List.of("Colchester Town PD", "Delaware County Sheriff", "Delhi Vg PD", "Hancock Vg PD", "Sidney Vg PD", "SUNY Police - Delhi", "Walton Vg PD")),
+    DUTCHESS(List.of("Beacon City PD", "Dutchess County Sheriff", "East Fishkill Town PD", "Fishkill Town PD", "Fishkill Vg PD", "Hyde Park Town PD", "Millbrook Vg PD", "Millerton Vg PD", "Pine Plains Town PD", "Poughkeepsie City PD", "Poughkeepsie Town PD", "Red Hook Vg PD", "Rhinebeck Vg PD", "Wappingers Falls Vg PD")),
+    ERIE(List.of("Akron Vg PD", "Amherst Town PD", "Brant Town PD", "Buffalo City PD", "Cheektowaga Town PD", "Depew Vg PD", "East Aurora-Town Of Aurora PD", "Eden Town PD", "Erie County Sheriff", "Evans Town PD", "Gowanda Vg PD", "Hamburg Town PD", "Hamburg Vg PD", "Kenmore Vg PD", "Lackawanna City PD", "Lancaster Town PD", "Niagara Frontier Transportation Authority PD", "Orchard Park Town PD", "SUNY Police - Buffalo", "SUNY Police - Buffalo State College", "Tonawanda City PD", "Tonawanda Town PD", "West Seneca Town PD")),
+    ESSEX(List.of("Essex County Sheriff", "Lake Placid Vg PD", "Moriah Town PD", "Ticonderoga Town PD")),
+    FRANKLIN(List.of("Franklin County Sheriff", "Malone Vg PD", "Saranac Lake Vg PD", "Tupper Lake Vg PD")),
+    FULTON(List.of("Fulton County Sheriff", "Gloversville City PD", "Johnstown City PD", "Northville Vg PD")),
+    GENESEE(List.of("Batavia City PD", "Genesee County Sheriff", "LeRoy Vg PD")),
+    GREENE(List.of("Athens Vg PD", "Cairo Town PD", "Catskill Vg PD", "Coxsackie Vg PD", "Durham Town PD", "Greene County Sheriff", "Hunter Town PD", "Windham Town PD")),
+    HAMILTON(List.of("Hamilton County Sheriff", "Inlet Town PD")),
+    HERKIMER(List.of("Dolgeville Vg PD", "Frankfort Town PD", "Frankfort Vg PD", "Herkimer County Sheriff", "Herkimer Vg PD", "Ilion Vg PD", "Little Falls City PD", "Mohawk Vg PD", "Webb Town PD")),
+    JEFFERSON(List.of("Adams Vg PD", "Alexandria Bay Vg PD", "Black River Vg PD", "Cape Vincent Vg PD", "Carthage Vg PD", "Clayton Vg PD", "Dexter Vg PD", "Glen Park Vg PD", "Jefferson County Sheriff", "Sackets Harbor Vg PD", "Theresa Vg PD", "Watertown City PD", "West Carthage Vg PD")),
+    LEWIS(List.of("Lewis County Sheriff", "Lowville Vg PD")),
+    LIVINGSTON(List.of("Avon Vg PD", "Caledonia Vg PD", "Dansville Vg PD", "Geneseo Vg PD", "Livingston County Sheriff", "Mount Morris Vg PD", "Nunda Town and Vg PD", "SUNY Police - Geneseo")),
+    MADISON(List.of("Canastota Vg PD", "Cazenovia Vg PD", "Chittenango Vg PD", "Hamilton Vg PD", "Madison County Sheriff", "Oneida City PD", "SUNY Police - Morrisville")),
+    MONROE(List.of("Brighton Town PD", "Brockport Vg PD", "East Rochester Vg PD", "Fairport Vg PD", "Gates Town PD", "Greece Town PD", "Irondequoit Town PD", "Monroe County Sheriff", "Ogden Town PD", "Rochester City PD", "SUNY Police - Brockport", "Webster Town and Vg PD")),
+    MONTGOMERY(List.of("Amsterdam City PD", "Canajoharie Vg PD", "Fort Plain Vg PD", "Montgomery County Sheriff", "St Johnsville Vg PD")),
+    NASSAU(List.of("Centre Island Vg PD", "Floral Park Vg PD", "Freeport Vg PD", "Garden City Vg PD", "Glen Cove City PD", "Great Neck Estates Vg PD", "Hempstead Vg PD", "Kensington Vg PD", "Kings Point Vg PD", "Lake Success Vg PD", "Long Beach City PD", "Lynbrook Vg PD", "Malverne Vg PD", "Nassau County PD", "Old Brookville Vg PD", "Old Westbury Vg PD", "Oyster Bay Cove Vg PD", "Port Washington Police District", "Rockville Centre Vg PD", "Sands Point Vg PD", "SUNY Police - Old Westbury")),
+    NEW_YORK_CITY(List.of("New York City Police Department", "SUNY Police - Downstate Medical", "SUNY Police - Maritime", "SUNY Police - Optometry")),
+    NIAGARA(List.of("Barker Vg PD", "Lewiston Town PD", "Lockport City PD", "Middleport Vg PD", "Niagara County Sheriff", "Niagara Falls City PD", "Niagara Town PD", "North Tonawanda City PD", "Youngstown Vg PD")),
+    ONEIDA(List.of("Boonville Vg PD", "Camden Vg PD", "Kirkland Town PD", "New Hartford Town and Vg PD", "New York Mills Vg PD", "Oneida County Sheriff", "Oriskany Vg PD", "Rome City PD", "Sherrill City PD", "SUNY Police - Polytechnic Institi", "Utica City PD", "Vernon Vg PD", "Whitesboro Vg PD", "Whitestown Town PD", "Yorkville Vg PD")),
+    ONONDAGA(List.of("Baldwinsville Vg PD", "Camillus Town and Vg PD", "Cicero Town PD", "DeWitt Town PD", "Geddes Town PD", "Jordan Vg PD", "Liverpool Vg PD", "Manlius Town PD", "Marcellus Vg PD", "North Syracuse Vg PD", "Onondaga County Park Rangers", "Onondaga County Sheriff", "Skaneateles Vg PD", "Solvay Vg PD", "SUNY Police - Environmental Science", "SUNY Police - Upstate Medical", "Syracuse City PD")),
+    ONTARIO(List.of("Canandaigua City PD", "Clifton Springs Vg PD", "Geneva City PD", "Manchester Vg PD", "Ontario County Sheriff", "Phelps Vg PD", "Shortsville Vg PD")),
+    ORANGE(List.of("Blooming Grove Town PD", "Chester Town PD", "Chester Vg PD", "Cornwall Town PD", "Cornwall-on-Hudson Vg PD", "Crawford Town PD", "Deerpark Town PD", "Florida Vg PD", "Goshen Town PD", "Goshen Vg PD", "Greenwood Lake Vg PD", "Harriman Vg PD", "Highland Falls Vg PD", "Highlands Town PD", "Maybrook Vg PD", "Middletown City PD", "Monroe Vg PD", "Montgomery Town PD", "Montgomery Vg PD", "Mount Hope Town PD", "New Windsor Town PD", "Newburgh City PD", "Newburgh Town PD", "Orange County Sheriff", "Port Jervis City PD", "Tuxedo Park Vg PD", "Tuxedo Town PD", "Walden Vg PD", "Wallkill Town PD", "Warwick Town PD", "Washingtonville Vg PD", "Woodbury Town PD")),
+    ORLEANS(List.of("Albion Vg PD", "Holley Vg PD", "Medina Vg PD", "Orleans County Sheriff")),
+    OSWEGO(List.of("Central Square Vg PD", "Fulton City PD", "Oswego City PD", "Oswego County Sheriff", "Phoenix Vg PD", "Pulaski Vg PD", "SUNY Police - Oswego")),
+    OTSEGO(List.of("Cooperstown Vg PD", "Oneonta City PD", "Otsego County Sheriff", "SUNY Police - Oneonta")),
+    PUTNAM(List.of("Brewster Vg PD", "Carmel Town PD", "Cold Spring Vg PD", "Kent Town PD", "Putnam County Sheriff")),
+    RENSSELAER(List.of("East Greenbush Town PD", "Hoosick Falls Vg PD", "Nassau Vg PD", "North Greenbush Town PD", "Rensselaer City PD", "Rensselaer County Sheriff", "Rensselaer Poly Tech Police", "Schodack Town PD", "Troy City PD")),
+    ROCKLAND(List.of("Clarkstown Town PD", "Haverstraw Town PD", "Orangetown Town PD", "Piermont Vg PD", "Ramapo Town PD", "Rockland County Sheriff", "South Nyack-Grand View PD", "Spring Valley Vg PD", "Stony Point Town PD", "Suffern Va PD")),
+    ST_LAWRENCE(List.of("Canton Vg PD", "Gouverneur Vg PD", "Massena Vg PD", "Norfolk Town PD", "Norwood Vg PD", "Ogdensburg City PD", "Potsdam Vg PD", "St Lawrence County Sheriff", "SUNY Police - Canton", "SUNY Police - Potsdam")),
+    SARATOGA(List.of("Ballston Spa Vg PD", "Galway Vg PD", "Mechanicville City PD", "Saratoga County Sheriff", "Saratoga Springs City PD", "South Glens Falls Vg PD", "Stillwater Town PD", "Waterford Town and Vg PD")),
+    SCHENECTADY(List.of("Glenville Town PD", "Niskayuna Town PD", "Rotterdam Town PD", "Schenectady City PD", "Schenectady County Sheriff", "Scotia Va PD")),
+    SCHOHARIE(List.of("Cobleskill Vg PD", "Schoharie County Sheriff", "Schoharie Vg PD", "SUNY Police - Cobleskill")),
+    SCHUYLER(List.of("Schuyler County Sheriff", "Watkins Glen Vg PD")),
+    SENECA(List.of("Interlaken Vg PD", "Seneca County Sheriff", "Seneca Falls Town PD", "Waterloo Vg PD")),
+    STEUBEN(List.of("Addison Vg PD", "Bath Vg PD", "Canisteo Vg PD", "Corning City PD", "Hammondsport Vg PD", "Hornell City PD", "Painted Post Vg PD", "Steuben County Sheriff", "Wavland Va PD")),
+    SUFFOLK(List.of("Amityville Vg PD", "Asharoken Vg PD", "East Hampton Town PD", "East Hampton Vg PD", "Head-of-the-Harbor Vg PD", "Huntington Bay Vg PD", "Lloyd Harbor Vg PD", "Nissequogue Vg PD", "Northport Vg PD", "Ocean Beach Vg PD", "Quogue Vg PD", "Riverhead Town PD", "Sag Harbor Vg PD", "Shelter Island Town PD", "Southampton Town PD", "Southampton Vg PD", "Southold Town PD", "Suffolk County PD", "Suffolk County Sheriff", "SUNY Police - Farmingdale", "SUNY Police - Stony Brook", "Westhampton Beach Vg PD")),
+    SULLIVAN(List.of("Fallsburg Town PD", "Liberty Vg PD", "Monticello Vg PD", "Sullivan County Sheriff", "Woodridge Vg PD")),
+    TIOGA(List.of("Owego Vg PD", "Tioga County Sheriff", "Waverly Vg PD")),
+    TOMPKINS(List.of("Cayuga Heights Vg PD", "Cornell University Police", "Dryden Vg PD", "Groton Vg PD", "Ithaca City PD", "Ithaca College PD", "Tompkins County Sheriff", "Trumansburg Vg PD")),
+    ULSTER(List.of("Ellenville Vg PD", "Kingston City PD", "Lloyd Town PD", "Marlborough Town PD", "New Paltz Town and Vg PD", "NYC DEPP - Ashokan PCT", "Olive Town PD", "Plattekill Town PD", "Rosendale Town PD", "Saugerties Town PD", "Shandaken Town PD", "Shawangunk Town PD", "SUNY Police - New Paltz", "Ulster County Sheriff", "Ulster Town PD", "Woodstock Town PD")),
+    WARREN(List.of("Bolton Town PD", "Glens Falls City PD", "Warren County Sheriff")),
+    WASHINGTON(List.of("Cambridge Vg PD", "Fort Edward Vg PD", "Granville Vg PD", "Greenwich Vg PD", "Hudson Falls Vg PD", "Washington County Sheriff", "Whitehall Vg PD")),
+    WAYNE(List.of("Clyde Vg PD", "Macedon Town and Vg PD", "Newark Vg PD", "Palmyra Vg PD", "Sodus Point Vg PD", "Sodus Vg PD", "Wayne County Sheriff", "Wolcott Vg PD"));
+
+    private List<String> policeDepartments;
+
+    private Counties(List<String> policeDepartments) {
+        this.policeDepartments = policeDepartments;
+    }
+
+    public List<String> getPD() {
+        return policeDepartments;
+    }
+
+    public String findCountyName(String pdName) {
+        for (Counties county : Counties.values()) {
+            if (county.getPD().contains(pdName)) {
+                return county.name();
+            }
+        }
+        return "County not found";
+        
+    }
+
+}
+
